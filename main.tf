e4967b55f31d157d3f453afbb37620c98b9923c2
Amit Bisht <amit.bisht@dishtv.in>
2025-08-15 21:27:18 +0530

    added new rg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "tinku-rg"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "branch1" {
+  name     = "branch1"
+  location = "East US"
+}
